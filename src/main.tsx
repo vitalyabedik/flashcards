9c2c12443c6a9f66961efd8ca3bffebeb6906189
Vitali Bedik <vitalya.bedik@gmail.com>
2023-09-12 21:28:31 +0300

    refactor: use alias for styles, delete strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
-import './styles/index.scss'
-
-import { StrictMode } from 'react'
+import '@styles/index.scss'
 
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+createRoot(document.getElementById('root') as HTMLElement).render(<App />)
