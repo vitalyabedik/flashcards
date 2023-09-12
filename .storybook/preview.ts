2fdda5649262443ba0f2a08f3768c1d3f314c0d5
Denis Kulik <kulikdenis1996x@gmail.com>
2023-09-12 12:58:12 +0700

    feat: add basic styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
+import '../src/styles/index.scss'
 
 import type { Preview } from '@storybook/react'
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
-
-import './index.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
