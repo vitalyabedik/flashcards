82ca0fa32aa17c7d775d099c66c9889dcf4c8252
Vitali Bedik <vitalya.bedik@gmail.com>
2023-09-14 22:30:59 +0300

    feat: create Typography component

--- src/common/components/typography/Typography.tsx
+++ src/common/components/typography/Typography.tsx
@@ -1,5 +1,31 @@
-type Props = {}
+import { ComponentPropsWithoutRef, ElementType } from 'react'
 
-export const Typography = ({}: Props): JSX.Element => {
-  return <div></div>
+import cn from 'classnames'
+
+import s from './Typography.module.scss'
+
+export type TypographyProps<T extends ElementType = 'p'> = {
+  as?: T
+  variant?:
+    | 'large'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'body1'
+    | 'body2'
+    | 'subtitle1'
+    | 'subtitle2'
+    | 'caption'
+    | 'overline'
+    | 'link1'
+    | 'link2'
+} & ComponentPropsWithoutRef<T>
+
+export const Typography = <T extends ElementType = 'p'>(
+  props: TypographyProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof TypographyProps<T>>
+): JSX.Element => {
+  const { variant = 'body1', className, as: Component = 'p', ...rest } = props
+  const typographyClasses = cn(s[variant], className)
+
+  return <Component className={typographyClasses} {...rest} />
 }
