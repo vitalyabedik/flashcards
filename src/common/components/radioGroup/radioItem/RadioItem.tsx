82cf07475955384b571b97f313c60c9f4b9eceb5
VitaliKozhurov <vitalik24021992@gmail.com>
2023-09-17 21:51:00 +0300

    refactor: add Typography component for RadioItem component

--- src/common/components/radioGroup/radioItem/RadioItem.tsx
+++ src/common/components/radioGroup/radioItem/RadioItem.tsx
@@ -3,7 +3,10 @@ import { JSX } from 'react'
 import * as Radio from '@radix-ui/react-radio-group'
 import cn from 'classnames'
 
+import { Typography } from '@common/components'
 import s from '@common/components/radioGroup/radioItem/RadioItem.module.scss'
+import { TypographyVariant } from '@common/enums'
+
 export type RadioItem = {
   value: string
   labelTitle: string
@@ -12,12 +15,16 @@ export type RadioItem = {
 export const RadioElement = ({ value, labelTitle, disabled }: RadioItem): JSX.Element => {
   return (
     <div className={s.itemContainer}>
-      <label className={cn(s.label, disabled && s.disabled)}>
+      <Typography
+        as={'label'}
+        variant={TypographyVariant.Body2}
+        className={cn(s.label, disabled && s.disabled)}
+      >
         <Radio.Item className={s.item} value={value} disabled={disabled}>
           <Radio.Indicator className={s.indicator} />
         </Radio.Item>
         <span>{labelTitle}</span>
-      </label>
+      </Typography>
     </div>
   )
 }
