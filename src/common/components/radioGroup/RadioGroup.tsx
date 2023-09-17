34122970f67e658d124d2a82b450107d51ad8549
VitaliKozhurov <vitalik24021992@gmail.com>
2023-09-17 20:30:25 +0300

    refactor: refactor Radio.Root, add forwardRef

--- src/common/components/radioGroup/RadioGroup.tsx
+++ src/common/components/radioGroup/RadioGroup.tsx
@@ -1,32 +1,31 @@
-import { JSX } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, JSX } from 'react'
 
 import * as Radio from '@radix-ui/react-radio-group'
+import cn from 'classnames'
 
 import s from './RadioGroup.module.scss'
 
 import { RadioElement, RadioItem } from '@common/components/radioGroup/radioItem/RadioItem.tsx'
 
-type RadioGroupProps = {
-  value: string
-  radioGroupItems: RadioItem[]
-  onValueChange: (value: string) => void
-}
+type RadioGroupProps = { radioGroupItems: RadioItem[] } & ComponentPropsWithoutRef<
+  typeof Radio.Root
+>
 
-export const RadioGroup = ({
-  value,
-  radioGroupItems,
-  onValueChange,
-}: RadioGroupProps): JSX.Element => {
-  return (
-    <Radio.Root
-      className={s.root}
-      onValueChange={onValueChange}
-      value={value}
-      defaultValue={radioGroupItems[0].value}
-    >
-      {radioGroupItems.map(item => (
-        <RadioElement key={item.labelTitle} {...item} />
-      ))}
-    </Radio.Root>
-  )
-}
+export const RadioGroup = forwardRef<ElementRef<typeof Radio.Root>, RadioGroupProps>(
+  ({ value, radioGroupItems, onValueChange, className, ...props }, ref): JSX.Element => {
+    const rootClassname = cn(s.root, className)
+
+    return (
+      <Radio.Root
+        ref={ref}
+        className={rootClassname}
+        defaultValue={radioGroupItems[0].value}
+        {...props}
+      >
+        {radioGroupItems.map(item => (
+          <RadioElement key={item.labelTitle} {...item} />
+        ))}
+      </Radio.Root>
+    )
+  }
+)
