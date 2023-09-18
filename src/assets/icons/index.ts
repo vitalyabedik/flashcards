9f90f083e14909e4c213ac9707995bf113a474d3
Denis Kulik <kulikdenis1996x@gmail.com>
2023-09-18 16:42:51 +0700

    chore: configure icons to the project

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as More } from './More'
+export { default as PlayCircle } from './PlayCircle'
+export { default as Edit } from './Edit'
+export { default as Delete } from './Delete'
