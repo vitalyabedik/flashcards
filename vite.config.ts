6f17b6edc5ffba49793a8683b86496972a3932bb
Vitali Bedik <vitalya.bedik@gmail.com>
2023-09-25 19:07:52 +0300

    feat: add alias for @components/*

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
       { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
       { find: '@common', replacement: path.resolve(__dirname, 'src/common') },
       { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
     ],
