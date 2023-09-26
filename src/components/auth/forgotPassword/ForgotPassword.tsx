4cc529db7336ac66daf41767376caba0d34d4e9e
VitaliKozhurov <vitalik24021992@gmail.com>
2023-09-26 22:20:38 +0300

    feat: add stories and styles

--- src/components/auth/forgotPassword/ForgotPassword.tsx
+++ src/components/auth/forgotPassword/ForgotPassword.tsx
@@ -1,26 +1,30 @@
 import { useForm } from 'react-hook-form'
 
+import s from './ForgotPassword.module.scss'
+
 import { TypographyVariant } from '@/common'
-import { Button, Input, Typography } from '@/components'
+import { Button, Card, Input, Typography } from '@/components'
 
 export const ForgotPassword = () => {
   const {} = useForm()
 
   return (
-    <form>
-      <Input />
-      <Typography as="p" variant={TypographyVariant.Body2}>
-        Enter your email address and we will send you further instructions
-      </Typography>
-      <Button variant="primary" fullWidth>
-        Send Instructions
-      </Button>
-      <Typography as="p" variant={TypographyVariant.Body2}>
-        Did you remember your password?
-      </Typography>
-      <Button as="a" variant="link">
-        Try logging in
-      </Button>
-    </form>
+    <Card className={s.formWrapper}>
+      <form className={s.form}>
+        <Input />
+        <Typography as="p" variant={TypographyVariant.Body2}>
+          Enter your email address and we will send you further instructions
+        </Typography>
+        <Button variant="primary" fullWidth>
+          Send Instructions
+        </Button>
+        <Typography as="p" variant={TypographyVariant.Body2}>
+          Did you remember your password?
+        </Typography>
+        <Button as="a" variant="link">
+          Try logging in
+        </Button>
+      </form>
+    </Card>
   )
 }
