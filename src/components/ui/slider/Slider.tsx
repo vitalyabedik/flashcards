52df50b43a959522d427ddc5c7225b3cf307dc11
Denis Kulik <kulikdenis1996x@gmail.com>
2023-09-27 19:03:25 +0700

    fix: slider props type

--- src/components/ui/slider/Slider.tsx
+++ src/components/ui/slider/Slider.tsx
@@ -8,27 +8,28 @@ import s from './Slider.module.scss'
 import { TypographyVariant } from '@/common'
 import { Typography } from '@/components'
 
-export const Slider = forwardRef<
-  ElementRef<typeof SliderPrimitive.Root>,
-  ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref): JSX.Element => {
-  const sliderClasses = cn(s.root, className)
+export type SliderProps = ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 
-  return (
-    <div className={s.container}>
-      <Typography variant={TypographyVariant.Body1} className={s.valueWrapper} as="div">
-        {props?.value?.[0]}
-      </Typography>
-      <SliderPrimitive.Root ref={ref} className={sliderClasses} {...props}>
-        <SliderPrimitive.Track className={s.track}>
-          <SliderPrimitive.Range className={s.range} />
-        </SliderPrimitive.Track>
-        <SliderPrimitive.Thumb className={s.thumb} aria-label="Value min" />
-        <SliderPrimitive.Thumb className={s.thumb} aria-label="Value max" />
-      </SliderPrimitive.Root>
-      <Typography variant={TypographyVariant.Body1} className={s.valueWrapper} as="div">
-        {props?.value?.[1]}
-      </Typography>
-    </div>
-  )
-})
+export const Slider = forwardRef<ElementRef<typeof SliderPrimitive.Root>, SliderProps>(
+  ({ className, ...props }, ref): JSX.Element => {
+    const sliderClasses = cn(s.root, className)
+
+    return (
+      <div className={s.container}>
+        <Typography variant={TypographyVariant.Body1} className={s.valueWrapper} as="div">
+          {props?.value?.[0]}
+        </Typography>
+        <SliderPrimitive.Root ref={ref} className={sliderClasses} {...props}>
+          <SliderPrimitive.Track className={s.track}>
+            <SliderPrimitive.Range className={s.range} />
+          </SliderPrimitive.Track>
+          <SliderPrimitive.Thumb className={s.thumb} aria-label="Value min" />
+          <SliderPrimitive.Thumb className={s.thumb} aria-label="Value max" />
+        </SliderPrimitive.Root>
+        <Typography variant={TypographyVariant.Body1} className={s.valueWrapper} as="div">
+          {props?.value?.[1]}
+        </Typography>
+      </div>
+    )
+  }
+)
