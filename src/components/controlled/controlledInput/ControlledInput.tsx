ecb291925133473b93e48ce4aade85b9a0600f05
VitaliKozhurov <vitalik24021992@gmail.com>
2023-09-27 22:57:38 +0300

    refactor: refactor props destructure, add return component type

--- src/components/controlled/controlledInput/ControlledInput.tsx
+++ src/components/controlled/controlledInput/ControlledInput.tsx
@@ -1,3 +1,5 @@
+import { JSX } from 'react'
+
 import { FieldValues, useController, UseControllerProps } from 'react-hook-form'
 
 import { Input, InputProps } from '@/components'
@@ -5,9 +7,11 @@ import { Input, InputProps } from '@/components'
 type ControlledInputProps<T extends FieldValues> = UseControllerProps<T> &
   Omit<InputProps, 'value' | 'onChange'>
 
-export const ControlledInput = <T extends FieldValues>(props: ControlledInputProps<T>) => {
-  const { name, control, ...restInputProps } = props
-
+export const ControlledInput = <T extends FieldValues>({
+  name,
+  control,
+  ...restInputProps
+}: ControlledInputProps<T>): JSX.Element => {
   const {
     field,
     fieldState: { error },
--- src/components/controlled/controlledRadioGroup/ControlledRadioGroup.tsx
+++ src/components/controlled/controlledRadioGroup/ControlledRadioGroup.tsx
@@ -1,3 +1,5 @@
+import { JSX } from 'react'
+
 import { FieldValues, useController, UseControllerProps } from 'react-hook-form'
 
 import { RadioGroup, RadioGroupProps } from '@/components'
@@ -5,10 +7,11 @@ import { RadioGroup, RadioGroupProps } from '@/components'
 type ControlledRadioGroupProps<T extends FieldValues> = UseControllerProps<T> &
   Omit<RadioGroupProps, 'value | onValueChange'>
 
-export const ControlledRadioGroup = <T extends FieldValues>(
-  props: ControlledRadioGroupProps<T>
-) => {
-  const { name, control, ...restProps } = props
+export const ControlledRadioGroup = <T extends FieldValues>({
+  name,
+  control,
+  ...restProps
+}: ControlledRadioGroupProps<T>): JSX.Element => {
   const { field } = useController({ name, control })
 
   return <RadioGroup {...field} {...restProps} />
