bddc6c0ae23828b183578717006f6d5d9e0829fa
Vitali Bedik <vitalya.bedik@gmail.com>
2023-09-27 16:57:29 +0300

    feat: add zod validation for SignIn form

--- src/components/auth/signIn/UseSignIn.ts
+++ src/components/auth/signIn/UseSignIn.ts
@@ -1,13 +1,24 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-export type FormValues = {
-  email: string
-  password: string
-  rememberMe: boolean
-}
+const signInSchema = z.object({
+  email: z.string().email({ message: 'Please enter a valid email address' }),
+  password: z.string().min(5, 'Use 5 characters or more for your password'),
+  rememberMe: z.boolean().default(false),
+})
+
+export type SignInFormValues = z.infer<typeof signInSchema>
 
 export const UseSignIn = () => {
-  const { register, control, handleSubmit } = useForm<FormValues>()
+  const { control, handleSubmit } = useForm<SignInFormValues>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
+  })
 
-  return { register, control, handleSubmit }
+  return { control, handleSubmit }
 }
