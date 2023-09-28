57c3cd259ec3764b224067bd1fe46915776fb96e
Denis Kulik <kulikdenis1996x@gmail.com>
2023-09-28 22:44:54 +0700

    chore: configure @storybook/addon-actions to the project

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@storybook/addon-actions',
   ],
   framework: {
     name: '@storybook/react-vite',
