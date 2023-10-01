e309d7ffa5e2129844435010235ce073c3499ad2
Denis Kulik <kulikdenis1996x@gmail.com>
2023-10-01 18:04:21 +0700

    refactor: add forwardRef for Button

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,4 +1,11 @@
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+import {
+  ComponentPropsWithoutRef,
+  ElementRef,
+  ElementType,
+  ForwardedRef,
+  forwardRef,
+  ReactNode,
+} from 'react'
 
 import cn from 'classnames'
 
@@ -12,11 +19,28 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   className?: string
 } & ComponentPropsWithoutRef<T>
 
-export const Button = <T extends ElementType = 'button'>(
-  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+export const ButtonPolymorph = <T extends ElementType = 'button'>(
+  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>,
+  ref: ElementRef<T>
 ): JSX.Element => {
-  const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+  const {
+    variant = 'primary',
+    fullWidth,
+    className,
+    as: Component = 'button',
+    ...restProps
+  } = props
   const buttonClasses = cn(s[variant], fullWidth && s.fullWidth, className)
 
-  return <Component className={buttonClasses} {...rest} />
+  return (
+    // @ts-expect-error TS2322
+    <Component ref={ref} className={buttonClasses} {...restProps} />
+  )
 }
+
+export const Button = forwardRef(ButtonPolymorph) as <T extends ElementType = 'button'>(
+  props: ButtonProps<T> &
+    Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>> & {
+      ref?: ForwardedRef<ElementRef<T>>
+    }
+) => ReturnType<typeof ButtonPolymorph>
