dc14ca60e89b0f937235c159f1fab342bdf80ee7
Denis Kulik <kulikdenis1996x@gmail.com>
2023-10-02 16:59:39 +0700

    feat: add LearnCard component

---
+++ src/features/decks/ui/learnCard/LearnCard.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+import s from './LearnCard.module.scss'
+
+import { TypographyVariant } from '@/common'
+import { Button, Card, Typography } from '@/components'
+import { RateLearnCard } from '@/features/decks/ui/learnCard/reteLearnCard'
+
+type Props = {
+  deck: {
+    id: string
+    name: string
+  }
+  card: {
+    id: string
+    question: string
+    answer: string
+    shots: number
+    answerImg?: string
+    questionImg?: string
+    questionVideo?: string
+    answerVideo?: string
+  }
+}
+
+export const LearnCard = ({ deck, card }: Props): JSX.Element => {
+  const [isShowAnswer, setIsShowAnswer] = useState(false)
+
+  const onShowAnswer = () => {
+    setIsShowAnswer(true)
+  }
+
+  const onSubmit = (data: unknown) => {
+    setIsShowAnswer(false)
+    console.log(data)
+  }
+
+  return (
+    <Card className={s.root}>
+      <Typography className={s.title} variant={TypographyVariant.Large} as="h2">
+        Learn {deck?.name}
+      </Typography>
+      <Typography className={s.question}>
+        <b>Question:</b> {card?.question}
+      </Typography>
+      <Typography className={s.shots} variant={TypographyVariant.Body2}>
+        Number of attempts: <b>{card?.shots}</b>
+      </Typography>
+
+      {isShowAnswer ? (
+        <>
+          <Typography className={s.answer}>
+            <b>Answer:</b> {card?.answer}
+          </Typography>
+          <Typography className={s.rate} variant={TypographyVariant.Subtitle1}>
+            Rate yourself:
+          </Typography>
+          <RateLearnCard onSubmit={onSubmit} />
+        </>
+      ) : (
+        <Button onClick={onShowAnswer} fullWidth>
+          Show Answer
+        </Button>
+      )}
+    </Card>
+  )
+}
---
+++ src/features/decks/ui/learnCard/reteLearnCard/rateOptions.ts
@@ -0,0 +1,9 @@
+import { RadioItemProps } from '@components/ui/radioGroup/radioItem'
+
+export const rateOptions: RadioItemProps[] = [
+  { title: 'Did not know', value: '1' },
+  { title: 'Forgot', value: '2' },
+  { title: 'A lot of thought', value: '3' },
+  { title: 'Confused', value: '4' },
+  { title: 'Knew the answer', value: '5' },
+]
