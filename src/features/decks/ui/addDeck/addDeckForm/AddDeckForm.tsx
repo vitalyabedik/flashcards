09e3769e3c05761d845a43a4009d9fb08b1d7307
VitaliKozhurov <vitalik24021992@gmail.com>
2023-10-02 16:35:54 +0300

    feat: add styles for addDeck component

--- src/features/decks/ui/addDeck/addDeckForm/AddDeckForm.tsx
+++ src/features/decks/ui/addDeck/addDeckForm/AddDeckForm.tsx
@@ -1,23 +1,28 @@
+import s from './AddDeckForm.module.scss'
+import { AddDeckFormValues, useAddDeck } from './useAddDeck'
+
 import { ButtonVariant, TypographyVariant } from '@/common'
 import { Button, ControlledCheckbox, ControlledInput, Typography } from '@/components'
-import { AddDeckProps } from '@/features/decks/ui/addDeck'
-import { useAddDeck } from '@/features/decks/ui/addDeck/addDeckForm/useAddDeck.ts'
 
-type AddDeckFormProps = { onClose: () => void } & Pick<AddDeckProps, 'onSubmit'>
+type AddDeckFormProps = {
+  onSubmit: (data: AddDeckFormValues) => void
+  onClose: () => void
+}
 
 export const AddDeckForm = ({ onSubmit, onClose }: AddDeckFormProps) => {
   const { control, handleSubmit } = useAddDeck()
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <ControlledInput
+    <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+      <ControlledInput className={s.input} control={control} name="newPackName" label="Name Pack" />
+      <ControlledCheckbox
+        className={s.checkbox}
         control={control}
-        name="newPackName"
-        placeholder="Name Pack"
-        label="Name Pack"
+        name="packPrivateState"
+        label="Private pack"
+        position="left"
       />
-      <ControlledCheckbox control={control} name="packPrivateState" label="Private pack" />
-      <div>
+      <div className={s.buttonsContainer}>
         <Button type="reset" variant={ButtonVariant.Secondary} onClick={onClose}>
           <Typography variant={TypographyVariant.Subtitle2}>Cancel</Typography>
         </Button>
