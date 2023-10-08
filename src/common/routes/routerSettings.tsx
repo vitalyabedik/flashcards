0c71b2017c99a414d80e8f234d7dd8a254b3bab4
Denis Kulik <kulikdenis1996x@gmail.com>
2023-10-08 18:21:17 +0700

    feat: add Profile page

--- src/common/routes/routerSettings.tsx
+++ src/common/routes/routerSettings.tsx
@@ -1,10 +1,10 @@
 import { RouteObject } from 'react-router-dom'
 
-import { CreateNewPassword, ForgotPassword, NotFoundPage } from '@/pages'
+import { CreateNewPassword, ForgotPassword, NotFoundPage, ProfilePage } from '@/pages'
 
 export const privateRoutes: RouteObject[] = [
   { path: '/', element: <div>Main page</div> },
-  { path: '/profile', element: <div>Profile page</div> },
+  { path: '/profile', element: <ProfilePage /> },
   { path: '/decks', element: <div>Decks page</div> },
   { path: '/decks/:id', element: <div>Deck page</div> },
 ]
