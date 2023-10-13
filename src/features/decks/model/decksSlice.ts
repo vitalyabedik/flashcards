930ab5bd4c37b6babcf54e581a8459a4fb2a145c
Vitali Bedik <vitalya.bedik@gmail.com>
2023-10-13 21:04:22 +0300

    feat: add decksSlice with types and connect to store

---
+++ src/features/decks/model/decksSlice.ts
@@ -0,0 +1,63 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+type InitialStateType = {
+  filter: FilterType
+  pagination: PaginationType
+}
+
+const initialState: InitialStateType = {
+  filter: {
+    searchByName: '',
+    tabValue: 'all',
+    sliderValues: [0, 10],
+  },
+  pagination: {
+    currentPage: 1,
+    pageSize: 10,
+    pageOptions: [
+      { value: '10', title: '10' },
+      { value: '20', title: '20' },
+      { value: '30', title: '30' },
+      { value: '40', title: '40' },
+      { value: '50', title: '50' },
+    ],
+  },
+}
+
+export const decksSlice = createSlice({
+  name: 'decks',
+  initialState,
+  reducers: {
+    setSearchByName: (state, action: PayloadAction<{ searchByName: string }>) => {
+      state.filter.searchByName = action.payload.searchByName
+    },
+    setCurrentPage: (state, action: PayloadAction<{ currentPage: number }>) => {
+      state.pagination.currentPage = action.payload.currentPage
+    },
+    setPageSize: (state, action: PayloadAction<{ pageSize: number }>) => {
+      state.pagination.pageSize = action.payload.pageSize
+    },
+  },
+})
+
+export const decksActions = decksSlice.actions
+
+// types
+type SliderValuesType = [number, number]
+
+type FilterType = {
+  searchByName: string
+  tabValue: string
+  sliderValues: SliderValuesType
+}
+
+type PageOptionType = {
+  value: string
+  title: string
+}
+
+type PaginationType = {
+  currentPage: number
+  pageSize: number
+  pageOptions: PageOptionType[]
+}
