0ee4ce7bc6aa0c28906e1a824499cc313f942a83
VitaliKozhurov <vitalik24021992@gmail.com>
2023-10-13 22:15:18 +0300

    refactor: add className prop for GoBack component

--- src/components/ui/goBack/GoBack.tsx
+++ src/components/ui/goBack/GoBack.tsx
@@ -1,5 +1,6 @@
 import { ElementRef, forwardRef } from 'react'
 
+import cn from 'classnames'
 import { useNavigate } from 'react-router-dom'
 
 import s from './GoBack.module.scss'
@@ -11,19 +12,22 @@ import { Button } from '@/components'
 type Props = {
   to?: Route
   title: string
+  className?: string
 }
 
 export const GoBack = forwardRef<ElementRef<typeof Button>, Props>(
-  ({ to, title }, ref): JSX.Element => {
+  ({ to, title, className }, ref): JSX.Element => {
     const navigate = useNavigate()
 
     const onBack = () => {
       to ? navigate(to) : navigate(-1)
     }
 
+    const goBackClassName = cn(s.root, className)
+
     return (
       // @ts-expect-error TS2322
-      <Button ref={ref} variant={ButtonVariant.Link} onClick={onBack} className={s.root}>
+      <Button ref={ref} className={goBackClassName} variant={ButtonVariant.Link} onClick={onBack}>
         <ArrowBackIcon size={1.6} />
         {title}
       </Button>
