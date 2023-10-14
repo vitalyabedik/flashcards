270825af080ced9b46c35ce901e88e67d503cf6f
Vitali Bedik <vitalya.bedik@gmail.com>
2023-10-14 17:42:05 +0300

    feat: add optimistic update for deleteDeck

--- src/features/decks/api/decksApi.ts
+++ src/features/decks/api/decksApi.ts
@@ -65,6 +65,41 @@ export const decksApi = baseApi.injectEndpoints({
         url: `decks/${id}`,
         method: 'DELETE',
       }),
+      async onQueryStarted({ id }, { dispatch, getState, queryFulfilled }) {
+        const state = getState() as RootState
+        const { searchName, authorId, sliderValues, sortOptions, currentPage, pageSize } =
+          state.decks
+
+        const sortedString = sortOptions ? `${sortOptions.key}-${sortOptions.direction}` : undefined
+
+        const patchResult = dispatch(
+          decksApi.util.updateQueryData(
+            'getDecks',
+            {
+              name: searchName,
+              authorId,
+              minCardsCount: String(sliderValues[0]),
+              maxCardsCount: String(sliderValues[1]),
+              orderBy: sortedString,
+              itemsPerPage: pageSize,
+              currentPage,
+            },
+            draft => {
+              const index = draft.items.findIndex(deck => deck.id === id)
+
+              if (index !== -1) {
+                draft.items.splice(index, 1)
+              }
+            }
+          )
+        )
+
+        try {
+          await queryFulfilled
+        } catch {
+          patchResult.undo()
+        }
+      },
       invalidatesTags: ['Decks'],
     }),
     updateDeck: builder.mutation<DeckType, UpdateDeckParamsType>({
