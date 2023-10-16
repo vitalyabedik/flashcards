bc3ad14531c9fdefbc91b16390bcdad27c55184d
Vitali Bedik <vitalya.bedik@gmail.com>
2023-10-16 16:54:27 +0300

    feat: use hooks useDecksFilters and useDecksPagination for DecksPage

--- src/pages/decksPage/DecksPage.tsx
+++ src/pages/decksPage/DecksPage.tsx
@@ -1,39 +1,35 @@
 import s from './DecksPage.module.scss'
 import { DecksPageHeader } from './decksPageHeader'
 
-import { formatSortedString, useAppDispatch, useAppSelector, useDebounce } from '@/common'
-import { Page, Pagination, Panel, Sort } from '@/components'
-import {
-  DecksTable,
-  selectSearchName,
-  selectTabValue,
-  selectSliderValues,
-  selectAuthorId,
-  selectSortOptions,
-  selectCurrentPage,
-  selectPageSize,
-  selectPageOptions,
-  useGetDecksQuery,
-  useMeQuery,
-  decksActions,
-} from '@/features'
+import { formatSortedString, useDebounce } from '@/common'
+import { Page, Pagination, Panel } from '@/components'
+import { DecksTable, useDecksPagination, useGetDecksQuery, useDecksFilters } from '@/features'
 
 export const DecksPage = (): JSX.Element => {
-  const searchName = useAppSelector(selectSearchName)
-  const tabValue = useAppSelector(selectTabValue)
-  const sliderValues = useAppSelector(selectSliderValues)
-  const authorId = useAppSelector(selectAuthorId)
-  const sortOptions = useAppSelector(selectSortOptions)
-  const currentPage = useAppSelector(selectCurrentPage)
-  const pageSize = useAppSelector(selectPageSize)
-  const pageOptions = useAppSelector(selectPageOptions)
+  const {
+    searchName,
+    tabValue,
+    sliderValues,
+    authorId,
+    sortOptions,
+    onSearchCallback,
+    onChangeTabValueCallback,
+    onChangeSliderValueCallback,
+    onChangeSortCallback,
+    onClearFilterCallback,
+  } = useDecksFilters()
 
-  const dispatch = useAppDispatch()
+  const {
+    currentPage,
+    pageSize,
+    pageOptions,
+    onChangePageSizeCallback,
+    onChangeCurrentPageCallback,
+  } = useDecksPagination()
 
   const debouncedSearchName = useDebounce(searchName, 1500)
   const sortedString = formatSortedString(sortOptions)
 
-  const { data: user } = useMeQuery()
   const { currentData: decks } = useGetDecksQuery({
     name: debouncedSearchName,
     authorId,
@@ -43,51 +39,6 @@ export const DecksPage = (): JSX.Element => {
     itemsPerPage: pageSize,
     currentPage,
   })
-
-  const onSearchCallback = (name: string) => {
-    dispatch(decksActions.setSearchByName({ searchName: name }))
-  }
-
-  const onChangeTabValueCallback = (tabValue: string) => {
-    dispatch(decksActions.setTabValue({ tabValue }))
-
-    if (tabValue === 'my') {
-      dispatch(decksActions.setAuthorId({ authorId: user?.id }))
-    } else {
-      dispatch(decksActions.setAuthorId({ authorId: undefined }))
-    }
-  }
-
-  const onChangeSliderValueCallback = (sliderValues: number[]) => {
-    dispatch(decksActions.setSliderValues({ sliderValues }))
-  }
-
-  const onChangeSortCallback = (orderBy: Sort) => {
-    dispatch(decksActions.setSortOptions({ sortOptions: orderBy }))
-  }
-
-  const onChangeCurrentPageCallback = (currentPage: number) => {
-    dispatch(decksActions.setCurrentPage({ currentPage }))
-  }
-
-  const onChangePageSizeCallback = (pageSize: string) => {
-    dispatch(decksActions.setPageSize({ pageSize: Number(pageSize) }))
-  }
-
-  const onClearFilter = () => {
-    dispatch(decksActions.setSearchByName({ searchName: '' }))
-    dispatch(decksActions.setTabValue({ tabValue: 'all' }))
-    dispatch(decksActions.setSliderValues({ sliderValues: [0, 10] }))
-    dispatch(decksActions.setAuthorId({ authorId: undefined }))
-    dispatch(
-      decksActions.setSortOptions({
-        sortOptions: {
-          key: 'updated',
-          direction: 'asc',
-        },
-      })
-    )
-  }
 
   return (
     <Page className={s.root}>
@@ -104,7 +55,7 @@ export const DecksPage = (): JSX.Element => {
         maxSliderValue={10}
         sliderLabel="Number of cards"
         onChangeSliderValue={onChangeSliderValueCallback}
-        onClearFilter={onClearFilter}
+        onClearFilter={onClearFilterCallback}
       />
       {decks && <DecksTable decksData={decks} sort={sortOptions} onSort={onChangeSortCallback} />}
       {!!decks?.items.length && (
