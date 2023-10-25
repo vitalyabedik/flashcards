aa859a76d87b98e5254d9228ec077a66ec6bc616
Vitali Bedik <vitalya.bedik@gmail.com>
2023-10-25 21:14:13 +0300

    feat: calculate delay on useDebounce hook, prepare for slider

--- src/common/hooks/useDebounce.ts
+++ src/common/hooks/useDebounce.ts
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react'
 
-export const useDebounce = (value: string, delay: number) => {
-  const [debouncedValue, setDebouncedValue] = useState(value)
+export const useDebounce = <T>(value: T, customDelay?: number) => {
+  const calculateDelay = (value: T) => {
+    return value === '' ? 0 : 1500
+  }
+
+  const delay = customDelay !== undefined ? customDelay : calculateDelay(value)
+
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
 
   useEffect(() => {
     const timer = setTimeout(() => {
